643f64fd6c731eeb7a63607be20c33204e6e2c5f
Shea Phillips <shea.phillips@gmail.com>
2020-10-14 13:57:38 -0700

    Initial commit of org info module, refactored to be compatible with SEA LZ.

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
-//put your awesome Terraform code here.
+provider "aws" {
+	version = "~> 2.0"
+	region = "ca-central-1"
+}
+
+data "aws_organizations_organization" "org_root" {}
+
+//all of the OU's within the org
+data "aws_organizations_organizational_units" "org_ous" {
+	parent_id = data.aws_organizations_organization.org_root.roots[0].id
+}
+
+locals {
+	non_master_accounts = data.aws_organizations_organization.org_root.non_master_accounts[*]
+
+	core_ou = [for ou in data.aws_organizations_organizational_units.org_ous.children : ou if ou.name == "core"][0]
+	core_account_names = ["log-archive","Perimeter", "iam-security", "security", "shared-services", "SharedNetwork", "Operations"]
+	core_accounts = [ for account in local.non_master_accounts : account if contains(local.core_account_names, account.name)]
+
+	dev_ou = [for ou in data.aws_organizations_organizational_units.org_ous.children : ou if ou.name == "Dev"][0]
+	test_ou = [for ou in data.aws_organizations_organizational_units.org_ous.children : ou if ou.name == "Test"][0]
+	prod_ou = [for ou in data.aws_organizations_organizational_units.org_ous.children : ou if ou.name == "Prod"][0]
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,22 @@
-//module outputs should be defined and documented here.
+output "root" {
+	value = data.aws_organizations_organization.org_root
+}
+
+output "master_account" {
+	value = {
+		"id" = data.aws_organizations_organization.org_root.master_account_id
+		"email" = data.aws_organizations_organization.org_root.master_account_email
+	}
+}
+
+output "core_ou" {
+	value = local.core_ou
+}
+
+output "core_accounts" {
+	value = local.core_accounts
+}
+
+output "workload_ous" {
+	value = [local.dev_ou, local.test_ou, local.prod_ou]
+}
